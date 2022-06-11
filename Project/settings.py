dede46a04420edf2fc925bf74f05da6228de6be0
igiidos <igiidos@gmail.com>
2022-06-11 23:26:10 +0900

    rds 연결

--- Project/settings.py
+++ Project/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'awslepetitprince03!',
+            'HOST': 'caloriemade-db-instance.c8ztzgyr9mrv.ap-northeast-2.rds.amazonaws.com',
+            'PORT': 5432,
+        }
     }
-}
 
 
 # Password validation
